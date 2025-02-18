06ae8ed6179883a3ab3e8ffe8562895c23c4ed12
hoquocnam92 <hoquocnam92@gmail.com>
2025-02-18 14:26:03 +0700

    FIX DONE

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ const overlay = document.querySelector('#overlay');
 const bars = document.querySelector('.bar-btn');
 const sidebar = document.querySelector('#sidebar');
 const closeBars = document.querySelector('.close-btn');
-bars.addEventListener('click',()=>{
-    overlay.style.display='block';
-    sidebar.style.display='block';
-})
-overlay.addEventListener('click',()=>{
-    overlay.style.display='none';
-    sidebar.style.display='none';
-});
-closeBars.addEventListener('click',()=>{
-    overlay.style.display='none';
-    sidebar.style.display='none';
-});
+
+function toggleSidebar () {
+    [overlay ,  sidebar , closeBars].forEach((e)=>{
+        e.classList.toggle('active');
+    })
+}
+
+
+[bars, overlay , closeBars].forEach((e)=>e.addEventListener('click', toggleSidebar));
